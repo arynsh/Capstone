de6530ef2b5e2c9a54ce1752674593c17737ed89
Sharon Lee <sharonlee714@gmail.com>
2020-03-06 12:14:04 -0800

    connect firebase

--- react-firebase-authentication/src/components/Firebase/index.js
+++ react-firebase-authentication/src/components/Firebase/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import FirebaseContext from './Context';
+import Firebase from './Firebase';
+
 const Firebase = () => (
   <div>
     <h1>Firebase</h1>
   </div>
 );
 export default Firebase;
+export { FirebaseContext };
